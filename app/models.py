a990bb468ea72173b8fc7da85c50758cff28f88d
github_username <you@example.com>
2025-09-24 11:55:04 +0300

    some changes

--- app/models.py
+++ app/models.py
@@ -7,14 +7,23 @@ class Author(models.Model):
     second_name = models.CharField(max_length=200)
     biography = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.second_name
+
 class Book(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books", null=True)
     pub_date = models.DateField("date published")
     genre = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Favourite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favourited_by")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="favourites")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favourited_by", null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="favourites", null=True)
     added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.book.name
